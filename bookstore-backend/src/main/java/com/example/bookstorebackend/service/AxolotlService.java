8f346506ad8488e5ced8a6fb1e7b8fe04d3aa95d
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 10:16:35 -0400

    pluralized axolotl list

--- bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlService.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlService.java
@@ -4,6 +4,6 @@ import com.example.bookstorebackend.models.Axolotl;
 
 public interface AxolotlService {
     public Axolotl getAxolotl(); // display axolotl
-    public Iterable<Axolotl> listAxolotl(); // display list axolotl
+    public Iterable<Axolotl> listAxolotls(); // display list axolotl
 
 }
