5a670a1e9c945a9f584dd8bc5950763d8fde66bd
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 10:27:14 -0400

    replaced method content with axolotl service method calls that has all functionality from axolotlserviceimpl

--- bookstore-backend/src/main/java/com/example/bookstorebackend/controller/AxolotlController.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/controller/AxolotlController.java
@@ -1,6 +1,8 @@
 package com.example.bookstorebackend.controller;
 
 import com.example.bookstorebackend.models.Axolotl;
+import com.example.bookstorebackend.service.AxolotlService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,21 +12,16 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/axolotl")
 public class AxolotlController {
+    @Autowired // instead of making an object like AxolotlServiceImpl axolotlService = new AxolotlServiceImpl(), that an object from the axolotl class and has a few methods, but its directly impacted by the implementation which cause a problem (if service fails, you're screwed & it's harder to test because of the tightly coupled dependency)
+    AxolotlService axolotlService;
+
     @GetMapping // going to instantiate a new axolotl object and return it
     public Axolotl getAxolotl() {
-        Axolotl axolotlVarName = new Axolotl("Takis", 3);
-        return axolotlVarName;
+        return axolotlService.getAxolotl();
     }
 
     @GetMapping("/listall")
     public Iterable<Axolotl> listAxolotls() {
-        ArrayList axolotls = new ArrayList<Axolotl>();
-        axolotls.add(new Axolotl("Lupe", 6));
-        axolotls.add(new Axolotl("Ixtel", 12));
-        axolotls.add(new Axolotl("Angel", 2));
-        axolotls.add(new Axolotl("Queztal", 3));
-        axolotls.add(new Axolotl("Benito", 8));
-
-        return axolotls;
+        return axolotlService.listAxolotls();
     }
 }
