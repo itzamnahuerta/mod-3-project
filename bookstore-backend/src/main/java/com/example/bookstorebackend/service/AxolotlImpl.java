79b5ec3fbd84309b68aefe644998319bfe93e72b
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 10:08:54 -0400

    created implementation blueprint

--- bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlImpl.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlImpl.java
@@ -1,4 +1,18 @@
 package com.example.bookstorebackend.service;
 
-public class AxolotlImpl {
+import com.example.bookstorebackend.models.Axolotl;
+
+// this is java, add functionality to our axolotl service
+public class AxolotlImpl  implements  AxolotlService {
+
+    @Override
+    public Axolotl getAxolotl() {
+        Axolotl axolotlVarName = new Axolotl("Takis", 3);
+        return axolotlVarName;
+    }
+
+    @Override
+    public Iterable<Axolotl> listAxolotl() {
+        return null;
+    }
 }
