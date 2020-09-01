924bb96ee9dbd6cb2154da4b67ba188357e78e31
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 10:09:33 -0400

    will display one axolotl and list

--- bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlService.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlService.java
@@ -1,4 +1,9 @@
 package com.example.bookstorebackend.service;
 
+import com.example.bookstorebackend.models.Axolotl;
+
 public interface AxolotlService {
+    public Axolotl getAxolotl(); // display axolotl
+    public Iterable<Axolotl> listAxolotl(); // display list axolotl
+
 }
