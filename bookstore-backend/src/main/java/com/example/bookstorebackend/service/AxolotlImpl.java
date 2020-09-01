fa55b63e2d5164b29ff8c1d9f88ab3f3ee12e5f0
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 10:11:29 -0400

    added list content from axolotl controller

--- bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlImpl.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/service/AxolotlImpl.java
@@ -2,6 +2,8 @@ package com.example.bookstorebackend.service;
 
 import com.example.bookstorebackend.models.Axolotl;
 
+import java.util.ArrayList;
+
 // this is java, add functionality to our axolotl service
 public class AxolotlImpl  implements  AxolotlService {
 
@@ -12,7 +14,14 @@ public class AxolotlImpl  implements  AxolotlService {
     }
 
     @Override
-    public Iterable<Axolotl> listAxolotl() {
-        return null;
+    public Iterable<Axolotl> listAxolotls() {
+        ArrayList axolotls = new ArrayList<Axolotl>();
+        axolotls.add(new Axolotl("Lupe", 6));
+        axolotls.add(new Axolotl("Ixtel", 12));
+        axolotls.add(new Axolotl("Angel", 2));
+        axolotls.add(new Axolotl("Queztal", 3));
+        axolotls.add(new Axolotl("Benito", 8));
+
+        return axolotls;
     }
 }
