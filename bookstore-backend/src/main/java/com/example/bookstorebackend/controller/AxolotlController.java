ebec07585060cd9864c4a684cda8c28d77d15f28
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 09:57:03 -0400

    tested a list to a sub endpoint

--- bookstore-backend/src/main/java/com/example/bookstorebackend/controller/AxolotlController.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/controller/AxolotlController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/axolotl")
 public class AxolotlController {
@@ -13,5 +15,15 @@ public class AxolotlController {
         Axolotl axolotlVarName = new Axolotl("Takis", 3);
         return axolotlVarName;
     }
+    @GetMapping("/listall")
+    public Iterable<Axolotl> listCats() {
+        ArrayList axolotls = new ArrayList<Axolotl>();
+        axolotls.add(new Axolotl("Lupe", 6));
+        axolotls.add(new Axolotl("Ixtel", 12));
+        axolotls.add(new Axolotl("Angel", 2));
+        axolotls.add(new Axolotl("Queztal", 3));
+        axolotls.add(new Axolotl("Benito", 8));
 
+        return axolotls;
+    }
 }
