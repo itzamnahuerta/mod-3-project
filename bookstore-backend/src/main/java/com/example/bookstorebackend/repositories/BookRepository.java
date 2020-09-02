4ddbd30a77ab2f7196890a3b98aaa0ba46ac6872
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-02 09:33:29 -0400

    delete query

--- bookstore-backend/src/main/java/com/example/bookstorebackend/repositories/BookRepository.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/repositories/BookRepository.java
@@ -1,9 +1,12 @@
 package com.example.bookstorebackend.repositories;
 
 import com.example.bookstorebackend.models.Book;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
 
 // help us begin to interact with our database (CRUD)
 public interface BookRepository extends CrudRepository<Book, Long> {
-    
+    @Query(value = "delete from books where books.title = ?1", nativeQuery = true)
+    public HttpStatus deleteByTitle(String title);
 }
