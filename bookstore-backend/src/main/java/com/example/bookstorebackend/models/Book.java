76fb404c370005c9fee1444335e07fd533ade224
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 23:07:14 -0400

    -

--- bookstore-backend/src/main/java/com/example/bookstorebackend/models/Book.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/models/Book.java
@@ -1,32 +1,52 @@
 package com.example.bookstorebackend.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 // attach an entity annotation to connect to our database
 // but first add something to our pom.xlm
 @Entity
 @Table(name="books")
 public class Book {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
+
+    @Column(unique = true)
     private String title;
+
+    @Column
     private String author;
+
+    @Column
     private String plotSummary;
+
+    @Column
     private String publishingHouse;
+
+    @Column
     private String genre;
+
+    @Column
     private String coverImg;
+
+    @Column
     private Double price;
 
-    // shortcut to build constructor: command n => constructor => shift highlight => enter
-    public Book(String title, String author, String plotSummary, String publishingHouse, String genre, String coverImg, Double price) {
-        this.title = title;
-        this.author = author;
-        this.plotSummary = plotSummary;
-        this.publishingHouse = publishingHouse;
-        this.genre = genre;
-        this.coverImg = coverImg;
-        this.price = price;
+    public Book() {
+
     }
+    // shortcut to build constructor: command n => constructor => shift highlight => enter
+//    public Book(String title, String author, String plotSummary, String publishingHouse, String genre, String coverImg, Double price) {
+//        this.title = title;
+//        this.author = author;
+//        this.plotSummary = plotSummary;
+//        this.publishingHouse = publishingHouse;
+//        this.genre = genre;
+//        this.coverImg = coverImg;
+//        this.price = price;
+//    }
     // shortcut type "get" and "set"
     public String getTitle() {
         // getter
