e8579aa9fd5d30818e2000d5b09fe77dd4bbf7fc
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 23:07:36 -0400

    -

--- bookstore-backend/src/main/java/com/example/bookstorebackend/service/BookService.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/service/BookService.java
@@ -1,4 +1,10 @@
 package com.example.bookstorebackend.service;
 
+import com.example.bookstorebackend.models.Book;
+import org.springframework.http.HttpStatus;
+
 public interface BookService {
+    public Iterable<Book> listBooks();
+    Book createBook(Book book);
+    public HttpStatus deleteBook(Long id);
 }
