936acd221ce1133a1ccccd7a735961761028e213
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 23:09:07 -0400

    -

--- bookstore-backend/src/main/java/com/example/bookstorebackend/repositories/BookRepository.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/repositories/BookRepository.java
@@ -1,4 +1,9 @@
 package com.example.bookstorebackend.repositories;
 
-public interface BookRepository {
+import com.example.bookstorebackend.models.Book;
+import org.springframework.data.repository.CrudRepository;
+
+// help us begin to interact with our database (CRUD)
+public interface BookRepository extends CrudRepository<Book, Long> {
+    
 }
