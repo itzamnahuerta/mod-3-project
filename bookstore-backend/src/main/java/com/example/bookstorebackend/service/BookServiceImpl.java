0ade81694515c59fa48a53352bcebbae2eba4ed6
Itzamna Huerta <_@itzamna-huerta.local>
2020-09-01 23:08:39 -0400

    -

--- bookstore-backend/src/main/java/com/example/bookstorebackend/service/BookServiceImpl.java
+++ bookstore-backend/src/main/java/com/example/bookstorebackend/service/BookServiceImpl.java
@@ -1,4 +1,30 @@
 package com.example.bookstorebackend.service;
 
-public class BookServiceImpl {
+import com.example.bookstorebackend.models.Book;
+import com.example.bookstorebackend.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    BookRepository bookRepository; // will make one instance of the object = singleton
+
+    @Override
+    public Iterable<Book> listBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public HttpStatus deleteBook(Long id) {
+        bookRepository.deleteById(id);
+        return HttpStatus.OK;
+    }
 }
+
